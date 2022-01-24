64e0aff130dc9fcf874dc0e2ee159cfb8b1e45a7
darshana.kakade <darshana.kakade@congiwize.com>
2022-01-24 14:24:26 +0530

    addion of 2 numprogram added

---
+++ src/simpleJavaProgram/Add2num.java
@@ -0,0 +1,13 @@
+package simpleJavaProgram;
+
+public class Add2num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=10,b=10;
+    System.out.print(a+b);
+		
+	}
+	
+
+}
